fb2f3c8f9265c079f75cbca7f146e770f4c4f547
Prdhviattacker <prudhvisiva000@gmail.com>
2025-03-21 00:37:20 +0530

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,5 @@
+print("Hello World")
+print(os.environ.get('first_name'))
+print(os.environ.get('last_name'))
+print((os.environ.get('first_name'))=='Sagar')
+print((os.environ.get('last_name'))=='p')
